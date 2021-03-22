da321620d3a57cec5a86485ba413edf70dd01ef4
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2021-03-22 13:57:15 +0000

    implemented refresh token and email exists functionality on backend

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using Application.Auth.Commands;
 using Application.Auth.Dtos;
 using Application.Auth.Queries;
+using Application.Common.Exceptions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +34,28 @@ namespace API.Controllers
             SetTokenCookie(refreshToken);
 
             return Ok(userToReturn);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("emailExists")]
+        public async Task<ActionResult<bool>> CheckEmailExistence([FromQuery] CheckEmailExistenceQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refresh")]
+        public async Task<ActionResult<AuthUserForReturnDto>> RefreshToken(RefreshTokenDto command)
+        {
+            //Retrieve the refresh token from the cookies
+            var refreshTokenFromUser = Request.Cookies["refreshToken"];
+            if (refreshTokenFromUser == null)
+                throw new RestException(HttpStatusCode.Unauthorized);
+
+            //Pass the refresh token together with the dto to the command
+            var (result, refreshToken) = await Mediator.Send(new RefreshTokenCommand(command, refreshTokenFromUser));
+            SetTokenCookie(refreshToken);
+            return Ok(result);
         }
 
         private void SetTokenCookie(string refreshToken)
--- API/Controllers/ProductBrandsController.cs
+++ API/Controllers/ProductBrandsController.cs
@@ -1,9 +1,11 @@
 
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Brands.Commands;
 using Application.Brands.Dtos;
+using Application.Brands.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,6 +13,15 @@ namespace API.Controllers
     [Route("api/v{version:apiVersion}/productTypes/{id}/brands")]
     public class ProductBrandsController : BaseController
     {
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductBrandForReturnDto>>> GetProductBrands(Guid id)
+        {
+            var result = await Mediator.Send(new GetProductBrandsQuery { TypeId = id });
+            return Ok(result);
+
+        }
+
         [HttpPost]
         public async Task<ActionResult<ProductBrandForReturnDto>> CreateBrand(Guid id, ProductBrandForCreateDto brand)
         {
@@ -24,5 +35,8 @@ namespace API.Controllers
             var result = await Mediator.Send(new UpdateProductBrandCommand { TypeId = id, BrandId = brandId, Brand = brand });
             return Ok(result);
         }
+
+
+
     }
 }
